e4b074f5c011950bf9e808845f5f315bd025ec3f
jarjanazy <celil.cercenazi@gmail.com>
2021-04-03 18:22:01 +0300

    add the initial spring boot project with initial maven dependencies

---
+++ src/main/java/codeit/gatcha/GatchaApplication.java
@@ -0,0 +1,13 @@
+package codeit.gatcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeit/gatcha/GatchaApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.gatcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
