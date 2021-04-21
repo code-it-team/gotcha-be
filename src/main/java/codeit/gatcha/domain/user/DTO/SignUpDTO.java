dc1368227f1f5e5f2b658e5d2a9c3b57f436ac60
jarjanazy <celil.cercenazi@gmail.com>
2021-04-21 23:26:05 +0300

    Add constructors to the SignUpDTO

--- src/main/java/codeit/gatcha/domain/user/DTO/SignUpDTO.java
+++ src/main/java/codeit/gatcha/domain/user/DTO/SignUpDTO.java
@@ -1,10 +1,13 @@
 package codeit.gatcha.domain.user.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-@Data @RequiredArgsConstructor
+@Data @AllArgsConstructor
+@NoArgsConstructor
 public class SignUpDTO {
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
 }
