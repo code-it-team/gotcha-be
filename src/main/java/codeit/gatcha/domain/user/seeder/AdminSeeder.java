eea32792f646dfdcd17e02be1886b3b27e880d87
jarjanazy <celil.cercenazi@gmail.com>
2021-05-25 19:47:16 +0300

    Implement a new way for the AdminSeeder

--- src/main/java/codeit/gatcha/domain/user/seeder/AdminSeeder.java
+++ src/main/java/codeit/gatcha/domain/user/seeder/AdminSeeder.java
@@ -8,7 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import java.util.Arrays;
+
 import java.util.HashSet;
 
 @Component @Log4j2
@@ -24,29 +24,26 @@ public class AdminSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args){
-        if (dbNotAlreadyPopulated())
-            addAdmins();
-        else
-            log.info("DB Already Populated");
+        log.info("Trying To Adding New Users.....");
+
+        Authority adminAuthority = addAuthorityIfNotFoundOrGet("ROLE_ADMIN");
+        Authority userAuthority = addAuthorityIfNotFoundOrGet("ROLE_USER");
+
+        addUserIfNotFoundOrGet("jalil.jarjanazy@gmail.com", "testPass", adminAuthority);
+        addUserIfNotFoundOrGet("hazem.alabiad@gmail.com", "testPass", adminAuthority);
+        addUserIfNotFoundOrGet("test.test@gmail.com", "testPass", userAuthority);
     }
 
-    private boolean dbNotAlreadyPopulated() {
-        return userRepo.findByEmail("jalil.jarjanazy@gmail.com").isEmpty() &&
-                authorityRepo.findByRole("ROLE_ADMIN").isEmpty();
+    private Authority addAuthorityIfNotFoundOrGet(String role){
+        return authorityRepo.
+                findByRole(role).
+                orElseGet(() -> authorityRepo.save(new Authority(new HashSet<>(), role)));
     }
 
-    private void addAdmins() {
-        log.info("Adding new users.....");
-
-        Authority adminAuthority = new Authority(new HashSet<>(), "ROLE_ADMIN");
-        Authority userAuthority = new Authority(new HashSet<>(), "ROLE_USER");
-
-        authorityRepo.saveAll(Arrays.asList(adminAuthority, userAuthority));
-
-        GatchaUser jalil = new GatchaUser("jalil.jarjanazy@gmail.com", "testPass", true, adminAuthority);
-        GatchaUser hazem = new GatchaUser("hazem.alabiad@gmail.com", "testPass", true, adminAuthority);
-        GatchaUser testUser = new GatchaUser("test.test@gmail.com", "testPass", true, userAuthority);
-
-        userRepo.saveAll(Arrays.asList(jalil, hazem, testUser));
+    private void addUserIfNotFoundOrGet(String email, String password, Authority authority){
+        userRepo.
+                findByEmail("jalil.jarjanazy@gmail.com").
+                orElseGet(() -> userRepo.save(new GatchaUser(email, password, true, authority)));
     }
+
 }
