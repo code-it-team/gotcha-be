c4df42df32b49540eb18f92b52735fb776eeee29
celil <celil.cercenazi@gmail.com>
2022-03-03 13:05:57 +0300

    convert the controller test of QuestionControllerTest to a proper WebMvcTest

--- src/main/java/codeit/gatcha/api/DTO/question/inputDTO/NewQuestion_DTO.java
+++ src/main/java/codeit/gatcha/api/DTO/question/inputDTO/NewQuestion_DTO.java
@@ -1,10 +1,14 @@
 package codeit.gatcha.api.DTO.question.inputDTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotBlank;
 
-@Data @NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class NewQuestion_DTO {
     @NotBlank(message = "Question body can't be empty")
     private String questionBody;
--- src/test/java/codeit/gatcha/api/adminControllerTest/QuestionControllerTest.java
+++ src/test/java/codeit/gatcha/api/adminControllerTest/QuestionControllerTest.java
@@ -5,57 +5,97 @@ import codeit.gatcha.api.DTO.question.inputDTO.NewQuestion_DTO;
 import codeit.gatcha.api.DTO.question.outputDTO.Admin_QuestionDTO;
 import codeit.gatcha.api.DTO.question.outputDTO.Admin_QuestionsDTO;
 import codeit.gatcha.api.controller.QuestionController_Admin;
+import codeit.gatcha.api.service.question.API_QuestionDeletionService;
 import codeit.gatcha.api.service.question.API_QuestionFetchService;
+import codeit.gatcha.api.service.question.API_QuestionUpdateService;
 import codeit.gatcha.domain.question.entity.Question;
-import codeit.gatcha.domain.question.repo.QuestionRepo;
 import codeit.gatcha.domain.question.service.QuestionCreationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import java.util.Arrays;
 import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = QuestionController_Admin.class)
+@ContextConfiguration(classes = QuestionController_Admin.class)
+@WithMockUser
 public class QuestionControllerTest {
-    @Mock
+
+    @MockBean
     QuestionCreationService questionCreationService;
-    @Mock
-    QuestionRepo questionRepo;
+    @MockBean
+    API_QuestionFetchService api_questionFetchService;
+    @MockBean
+    API_QuestionDeletionService api_questionDeletionService;
+    @MockBean
+    API_QuestionUpdateService api_questionUpdateService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
-    void givenQuestionIsCreatedSuccessfully_CheckResponseMessage(){
-        QuestionController_Admin questionController_admin = new QuestionController_Admin(questionCreationService, null, null, null);
+    void givenQuestionIsCreatedSuccessfully_CheckResponseMessage() throws Exception {
+        NewQuestion_DTO question = new NewQuestion_DTO("test question");
 
-        NewQuestion_DTO question = new NewQuestion_DTO();
         doReturn(null).when(questionCreationService).createQuestion(any());
-        ResponseEntity<APIResponse> response = questionController_admin.createNewQuestion(question);
 
-        assertEquals(OK, response.getStatusCode());
-        assertEquals(OK.value(), response.getBody().getStatusCode());
-        assertEquals("The Question has been created successfully", response.getBody().getMessage());
+        MvcResult mvcResult = mockMvc.perform(post("/admin/createQuestion")
+                        .content(objectMapper.writeValueAsString(question))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        APIResponse apiResponse = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                APIResponse.class);
+
+        assertThat(apiResponse.getMessage()).isEqualTo("The Question has been created successfully");
     }
 
     @Test
-    void given_2_QuestionsInDB_GetTheirDTOs(){
-        API_QuestionFetchService api_questionFetchService = new API_QuestionFetchService(questionRepo, null, null, null);
-        QuestionController_Admin questionController_admin = new QuestionController_Admin(null, api_questionFetchService, null, null);
+    void given_2_QuestionsInDB_GetTheirDTOs() throws Exception {
+        Admin_QuestionDTO admin_questionDTO1 = new Admin_QuestionDTO(new Question(1, "q1"));
 
-        Question q1 = new Question(1, "q1");
-        Question q2 = new Question(2, "q2");
-        doReturn(Arrays.asList(q1, q2)).when(questionRepo).findQuestionsByValidTrue();
+        Admin_QuestionDTO admin_questionDTO2 = new Admin_QuestionDTO(new Question(2, "q2"));
 
-        ResponseEntity<APIResponse> result = questionController_admin.getAllValidQuestions();
-        Admin_QuestionsDTO questionsDTO = (Admin_QuestionsDTO) result.getBody().getBody();
+        Admin_QuestionsDTO admin_questionsDTO = new Admin_QuestionsDTO(Arrays.asList(admin_questionDTO1, admin_questionDTO2));
 
-        assertEquals(OK.value(), result.getStatusCodeValue());
-        assertEquals(OK, result.getStatusCode());
-        List<Admin_QuestionDTO> questions = questionsDTO.getQuestions();
+        doReturn(admin_questionsDTO).when(api_questionFetchService).getAllValidQuestions_DTO();
+
+        MvcResult mvcResult = mockMvc.perform(get("/admin/questions"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        APIResponse apiResponse = objectMapper.readValue(
+                mvcResult.getResponse().getContentAsString(),
+                APIResponse.class);
+
+        assertThat(apiResponse.getMessage()).isEqualTo("success");
+
+        Admin_QuestionsDTO body = objectMapper.convertValue(
+                apiResponse.getBody(),
+                Admin_QuestionsDTO.class);
+
+        List<Admin_QuestionDTO> questions = body.getQuestions();
 
         assertEquals(2, questions.size());
 
